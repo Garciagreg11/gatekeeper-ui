2e93708e05609a9417f21fcc4dc53ac6088c6459
Greg Garcia <garciagreg11@gmail.com>
2026-02-17 05:45:33 -0700

    Fix ABI import name

--- gatekeeper.ts
+++ gatekeeper.ts
@@ -1,13 +1,13 @@
 "use client";
 
 import { useReadContract } from "wagmi";
-import { gatekeeperAbi } from "@/lib/gatekeeperAbi";
+import { GATEKEEPER_ABI } from "@/lib/gatekeeperAbi";
 import { GATEKEEPER_ADDRESS } from "@/config";
 
 export function useGatekeeper() {
   const globalLimit = useReadContract({
     address: GATEKEEPER_ADDRESS,
-    abi: gatekeeperAbi,
+    abi: GATEKEEPER_ABI,
     functionName: "globalLimit",
   });
 
