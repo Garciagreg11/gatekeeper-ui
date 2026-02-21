0a5e8eed413d10db27c9f01bb33eb64bf1c66378
Greg Garcia <garciagreg11@gmail.com>
2026-02-21 12:37:59 -0700

    Fix constants.ts export and remove Edge metadata

--- constants.ts
+++ constants.ts
@@ -3,4 +3,5 @@
 export const GATEKEEPER_ADDRESS =
   "0x3F0c2937E1B4854E6Bb78AE5E7ce49AF3B42D84e";
 
-export { gatekeeperAbi } from "./gatekeeperAbi";
+export { GATEKEEPER_ABI } from "./gatekeeperAbi";
+
