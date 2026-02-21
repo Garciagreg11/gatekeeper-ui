9a2c7b2d9edf65039ac1d1d12e6871faa6a11ce9
Greg Garcia <garciagreg11@gmail.com>
2026-02-21 12:31:24 -0700

    Add clean Gatekeeper ABI

--- gatekeeperAbi.ts
+++ gatekeeperAbi.ts
@@ -1,91 +1,220 @@
 // gatekeeperAbi.ts
 
-export const gatekeeperAbi = [
+export const GATEKEEPER_ABI = [
   {
-    "type": "function",
-    "name": "globalDailyLimit",
-    "stateMutability": "view",
-    "inputs": [],
-    "outputs": [{ "type": "uint256" }]
-  },
-  {
-    "type": "function",
-    "name": "userDailyLimit",
-    "stateMutability": "view",
-    "inputs": [{ "name": "user", "type": "address" }],
-    "outputs": [{ "type": "uint256" }]
-  },
-  {
-    "type": "function",
-    "name": "setGlobalDailyLimit",
-    "stateMutability": "nonpayable",
-    "inputs": [{ "name": "newLimit", "type": "uint256" }],
-    "outputs": []
-  },
-  {
-    "type": "function",
-    "name": "setUserDailyLimit",
-    "stateMutability": "nonpayable",
     "inputs": [
-      { "name": "user", "type": "address" },
-      { "name": "newLimit", "type": "uint256" }
+      { "internalType": "address", "name": "_vault", "type": "address" },
+      { "internalType": "uint256", "name": "_globalDailyLimit", "type": "uint256" }
     ],
-    "outputs": []
-  },
-  {
-    "type": "function",
-    "name": "owner",
-    "stateMutability": "view",
-    "inputs": [],
-    "outputs": [{ "type": "address" }]
-  },
-  {
-    "type": "function",
-    "name": "transferOwnership",
     "stateMutability": "nonpayable",
-    "inputs": [{ "name": "newOwner", "type": "address" }],
-    "outputs": []
+    "type": "constructor"
   },
   {
-    "type": "event",
+    "anonymous": false,
+    "inputs": [
+      { "indexed": true, "internalType": "address", "name": "to", "type": "address" },
+      { "indexed": false, "internalType": "uint256", "name": "amount", "type": "uint256" }
+    ],
+    "name": "EmergencyWithdraw",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      { "indexed": true, "internalType": "address", "name": "user", "type": "address" },
+      { "indexed": false, "internalType": "uint256", "name": "amount", "type": "uint256" },
+      { "indexed": true, "internalType": "address", "name": "caller", "type": "address" }
+    ],
+    "name": "GasSponsored",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      { "indexed": false, "internalType": "uint256", "name": "oldLimit", "type": "uint256" },
+      { "indexed": false, "internalType": "uint256", "name": "newLimit", "type": "uint256" }
+    ],
     "name": "GlobalDailyLimitUpdated",
-    "inputs": [
-      { "name": "newLimit", "type": "uint256", "indexed": false }
-    ],
-    "anonymous": false
+    "type": "event"
   },
   {
-    "type": "event",
+    "anonymous": false,
+    "inputs": [
+      { "indexed": true, "internalType": "address", "name": "operator", "type": "address" },
+      { "indexed": false, "internalType": "bool", "name": "allowed", "type": "bool" }
+    ],
+    "name": "OperatorUpdated",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      { "indexed": true, "internalType": "address", "name": "oldOwner", "type": "address" },
+      { "indexed": true, "internalType": "address", "name": "newOwner", "type": "address" }
+    ],
+    "name": "OwnerChanged",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      { "indexed": false, "internalType": "bool", "name": "status", "type": "bool" }
+    ],
+    "name": "Paused",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      { "indexed": true, "internalType": "address", "name": "user", "type": "address" },
+      { "indexed": false, "internalType": "uint256", "name": "oldLimit", "type": "uint256" },
+      { "indexed": false, "internalType": "uint256", "name": "newLimit", "type": "uint256" }
+    ],
     "name": "UserDailyLimitUpdated",
+    "type": "event"
+  },
+  {
     "inputs": [
-      { "name": "user", "type": "address", "indexed": true },
-      { "name": "newLimit", "type": "uint256", "indexed": false }
+      { "internalType": "address[]", "name": "users", "type": "address[]" },
+      { "internalType": "uint256", "name": "amountPerUser", "type": "uint256" }
     ],
-    "anonymous": false
+    "name": "batchSponsorGas",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
   },
   {
-    "type": "event",
-    "name": "OwnershipTransferred",
+    "inputs": [{ "internalType": "address", "name": "", "type": "address" }],
+    "name": "dailySponsoredAmount",
+    "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [{ "internalType": "address", "name": "user", "type": "address" }],
+    "name": "effectiveDailyLimit",
+    "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
     "inputs": [
-      { "name": "previousOwner", "type": "address", "indexed": true },
-      { "name": "newOwner", "type": "address", "indexed": true }
+      { "internalType": "address", "name": "to", "type": "address" },
+      { "internalType": "uint256", "name": "amount", "type": "uint256" }
     ],
-    "anonymous": false
+    "name": "emergencyWithdraw",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
   },
   {
-    "type": "error",
-    "name": "NotOwner",
-    "inputs": []
+    "inputs": [],
+    "name": "globalDailyLimit",
+    "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }],
+    "stateMutability": "view",
+    "type": "function"
   },
   {
-    "type": "error",
-    "name": "NotGatekeeper",
-    "inputs": []
+    "inputs": [{ "internalType": "address", "name": "", "type": "address" }],
+    "name": "lastSponsoredDay",
+    "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }],
+    "stateMutability": "view",
+    "type": "function"
   },
   {
-    "type": "error",
-    "name": "InsufficientBalance",
-    "inputs": []
+    "inputs": [{ "internalType": "address", "name": "", "type": "address" }],
+    "name": "operators",
+    "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "owner",
+    "outputs": [{ "internalType": "address", "name": "", "type": "address" }],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "paused",
+    "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [{ "internalType": "address", "name": "user", "type": "address" }],
+    "name": "remainingToday",
+    "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [{ "internalType": "uint256", "name": "newLimit", "type": "uint256" }],
+    "name": "setGlobalDailyLimit",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      { "internalType": "address", "name": "operator", "type": "address" },
+      { "internalType": "bool", "name": "allowed", "type": "bool" }
+    ],
+    "name": "setOperator",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [{ "internalType": "address", "name": "newOwner", "type": "address" }],
+    "name": "setOwner",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [{ "internalType": "bool", "name": "_paused", "type": "bool" }],
+    "name": "setPaused",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      { "internalType": "address", "name": "user", "type": "address" },
+      { "internalType": "uint256", "name": "newLimit", "type": "uint256" }
+    ],
+    "name": "setUserDailyLimit",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      { "internalType": "address", "name": "user", "type": "address" },
+      { "internalType": "uint256", "name": "amount", "type": "uint256" }
+    ],
+    "name": "sponsorGas",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [{ "internalType": "address", "name": "", "type": "address" }],
+    "name": "userDailyLimit",
+    "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "vault",
+    "outputs": [
+      { "internalType": "contract IGatekeeperVault", "name": "", "type": "address" }
+    ],
+    "stateMutability": "view",
+    "type": "function"
   }
 ];
 
