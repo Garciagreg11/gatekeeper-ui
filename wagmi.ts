02e5976fdd5bf0169daed00d7c255e7ad07b3b64
Greg Garcia <garciagreg11@gmail.com>
2026-02-22 07:42:32 -0700

    Add wagmi config to project root

---
+++ wagmi.ts
@@ -0,0 +1,11 @@
+// wagmi.ts
+
+import { createConfig, http } from 'wagmi';
+import { baseSepolia } from 'wagmi/chains';
+
+export const config = createConfig({
+  chains: [baseSepolia],
+  transports: {
+    [baseSepolia.id]: http(),
+  },
+});
